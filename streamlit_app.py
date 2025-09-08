183573ea34230583eab88fb00534009a522b2914
zaslam14 <zaslam14@gmail.com>
2025-09-08 15:54:57 -0500

    Update streamlit_app.py
    added conversion of response to .json

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,4 @@ if ingredients_list:
 # New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
