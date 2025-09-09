0ad1853252e78675f450850c94a191073a5c8901
zaslam14 <zaslam14@gmail.com>
2025-09-09 10:51:30 -0500

    Update streamlit_app.py
    fixed typos calling pd_pf which should have been pd_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
-        search_on=pd_pf.loc[pd_pf['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chose, ' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
   
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
